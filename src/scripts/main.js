2750709162074fbec13b8d5d21f60d814fbdecc4
yegor-vin <egorvinvit@gmail.com>
2025-03-19 22:25:30 +0200

    problems with code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,67 @@
 'use strict';
 
-// write code here
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+
+const table = document.querySelector('body');
+const blueTable = document.querySelector('table');
+
+table.addEventListener('click', (e) => {
+  if (e.target === appendColumn) {
+    removeColumn.disabled = false;
+
+    document.querySelectorAll('tr').forEach((row) => {
+      const newCell = document.createElement('td');
+
+      row.appendChild(newCell);
+
+      if (row.querySelectorAll('td').length > 9) {
+        appendColumn.disabled = true;
+      }
+    });
+  }
+
+  if (e.target === removeColumn) {
+    appendColumn.disabled = false;
+
+    document.querySelectorAll('tr').forEach((row) => {
+      const lastChild = row.lastChild;
+
+      lastChild.remove();
+
+      if (row.querySelectorAll('td').length <= 2) {
+        removeColumn.disabled = true;
+      }
+    });
+  }
+
+  if (e.target === appendRow) {
+    removeRow.disabled = false;
+
+    const newRow = document.createElement('tr');
+
+    newRow.innerHTML = document.querySelector('tr').innerHTML;
+
+    blueTable.appendChild(newRow);
+
+    if (blueTable.querySelectorAll('tr').length > 9) {
+      appendRow.disabled = true;
+    }
+  }
+
+  if (e.target === removeRow) {
+    appendRow.disabled = false;
+
+    const lastRowChild = blueTable.lastChild;
+
+    lastRowChild.remove();
+
+    const rowsLeft = document.querySelectorAll('tr');
+
+    if (rowsLeft.length <= 2) {
+      removeRow.disabled = true;
+    }
+  }
+});
